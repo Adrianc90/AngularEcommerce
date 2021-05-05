4aef70c39fef281e9389bea0d3ae26878e472fa1
Adrianc90 <adco0211@gmail.com>
2021-05-05 15:51:41 -0500

    Adding Types and Brands

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,24 +12,38 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _repos;
+
+        public ProductsController(IProductRepository repos)
         {
-            _context = context;
+            _repos = repos;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var result = await _context.Products.ToListAsync<Product>();
+            var result = await _repos.GetProductsAsync();
             return Ok(result);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var result = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            var result = await _repos.GetProductByIdAsync(id);
             if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var result= await _repos.GetProductBrandsAsync();
+            return Ok(result);
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            var result= await _repos.GetProductTypesAsync();
             return Ok(result);
         }
     }
